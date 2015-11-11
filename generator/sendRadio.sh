70dab39c627f0b32983d54d248fd764b9d68980d
Toon Stegen <toon.stegen@altran.com>
2015-11-11 16:52:08 +0100

    Add new logs and generator

---
+++ generator/sendRadio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cansend can0 1A0#54.F8.C0.00.16.16.00.20
+sleep 0.09
+#cansend can0 1A0#14.28.C0.00.12.12.00.20
+#sleep 0.08
+cansend can0 1A0#7C.F8.C0.00.16.16.00.20
+#cansend can0 1A0#54.F8.C0.01.01.00.20
+#sleep 1
+#cansend can0 1A0#14.F8.C0.01.01.00.20
