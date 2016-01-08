1ab18d309a26bc57cc207c8bc3ad3627bc0f3ae0
Toon Stegen <toon.stegen@altran.com>
2016-01-08 10:53:54 +0100

    Make setup more robust and add isotp kernel module

--- setup.sh
+++ setup.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-if [ $# -ne 0 ]
-then
+function get_iface {
+    ifconfig $1 | sed 's/[ \t].*//;/^$/d' | grep can
+}
+
+if [ $# -ne 0 ]; then
     BITRATE=$1
 else
     BITRATE=500000
 fi
 
-# set the bitrate 
-sudo ip link set can0 type can bitrate $BITRATE
+# check if can kernel module is loaded
+if ! lsmod | grep '^can ' > /dev/null 2>&1; then
+    echo "Loading can kernel module..."
+    modprobe can
+fi
 
-# automatically recover from bus-off state
-sudo ip link set can0 type can restart-ms 100
+# check if ISO-TP kernel module is loaded
+if ! lsmod | grep '^can_isotp ' > /dev/null 2>&1; then
+    echo "Loading iso-tp kernel module..."
+    insmod can-isotp.ko
+fi
 
-# set the interface up
-sudo ip link set can0 up
+# check if interface exists
+if ! IFACE=$(get_iface -a); then
+    echo "No can interface found. Is the USB interface plugged in?"
+    exit 1
+else
+    echo "Found interface $iface"
+fi
+
+# check if iface is already up
+if get_iface > /dev/null 2>&1; then
+    echo "Already up"
+    exit 0
+else
+    echo "Seting up..."
+fi
+
+# configure restart
+sudo ip link set $IFACE type can restart-ms 100
+
+# configure baudrate
+sudo ip link set $IFACE up type can bitrate $BITRATE
+
+echo "Finished"
+
